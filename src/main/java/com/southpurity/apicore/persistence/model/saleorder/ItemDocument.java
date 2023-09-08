631200a90231e6e3177ff4beb6f7b4c1262ec723
Omar Gómez <omar.fdo.gomez@outlook.cl>
2023-09-08 11:14:04 -0300

    Set NotNull items attributes

--- src/main/java/com/southpurity/apicore/persistence/model/saleorder/ItemDocument.java
+++ src/main/java/com/southpurity/apicore/persistence/model/saleorder/ItemDocument.java
@@ -6,17 +6,21 @@ import com.southpurity.apicore.persistence.model.constant.CurrencyEnum;
 import lombok.Builder;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
+
 
 @Data
 @Builder
 public class ItemDocument {
 
+    @NotNull
     @JsonView(View.Customer.class)
     private Long quantity;
 
     @JsonView(View.Customer.class)
     private String name;
 
+    @NotNull
     @JsonView(View.Customer.class)
     private Long price;
 
